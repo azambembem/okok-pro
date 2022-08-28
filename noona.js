0221fa8ea2bcd0c056f2d9cf0a15099b514984bd
Azam <azamjan980713@gmail.com>
2022-08-28 23:30:56 +0900

    first commit

---
+++ noona.js
@@ -0,0 +1,12 @@
+// let name = "noona's fruit store";
+// let address = "seoul";
+
+// console.log(`제 가게 이름은 ${name}입니다. 위취는 ${address}에 있씁니다`)
+
+
+let calculate = (obj)=>{
+    let {a, b, c}=obj
+    return a+b+c
+}
+
+calculate({a:1, b:2, c:3})
